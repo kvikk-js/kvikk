66cf63f9af5f3d65366afb91befd42527e8686d2
Trygve Lie <trygve.lie@finn.no>
2024-06-03 13:09:28 +0200

    feat: Added metadata concept and load pages by hashes

--- system/document.js
+++ system/document.js
@@ -7,8 +7,8 @@ export default (page, metadata) => {
                 <meta charset="utf-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-                <script src="/foo/_/dynamic/modules/@lit-labs/ssr-client/lit-element-hydrate-support.js" type="module"></script>
-                <script src="/foo/_/dynamic/pages/page.js" type="module"></script>
+                <script src="/_/dynamic/modules/@lit-labs/ssr-client/lit-element-hydrate-support.js" type="module"></script>
+                <script src="/_/dynamic/pages/${metadata.pageScript}" type="module"></script>
                 <title>${metadata?.title}</title>
             </head>
             <body>
