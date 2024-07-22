cffe762c71d8e4153ded32ba4a9446561c6beb33
Trygve Lie <trygve.lie@finn.no>
2024-07-22 15:49:24 +0200

    test: Fix tests

--- test/integration/errors.js
+++ test/integration/errors.js
@@ -9,16 +9,6 @@ test('Integration', async (t) => {
   const app = await server({}, config);
   const address = await app.start();
 
-  await t.test('Route: ./', async () => {
-    const response = await fetch(address);
-
-    assert.equal(response.status, 200, 'Should result in an http status 200 response');
-    assert.equal(response.ok, true, 'Should result in an OK response');
-
-    // Body must be consumed, if not test will not exit
-    await response.text();
-  });
-
   await t.test('Route: ./error', async () => {
     const response = await fetch(new URL('./error', address));
 
